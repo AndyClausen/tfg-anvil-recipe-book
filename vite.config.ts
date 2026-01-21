dcb7bb0f27c4889c7bb187b3bfa1c384f933f461
Andy <andreas.clausen@hotmail.com>
2026-01-21 09:32:00 +0100

    Fix project name typo in configuration: repice -> recipe

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tfg-anvil-repice-book/',
+  base: '/tfg-anvil-recipe-book/',
   plugins: [
     react(),
     tailwindcss(),
